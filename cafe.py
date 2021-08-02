2f835f66c7cc333d735eae1aa1526133461c1140
cooldudeguyman <liamgray0912@gmail.com>
2021-08-02 12:16:16 +0800

    test commit

---
+++ cafe.py
@@ -0,0 +1,12 @@
+#code = 01000101010001010100010010101
+
+special = 1
+totals{
+    "total orders": 0,
+    "total days income": 0,
+    "total GST"
+}
+while special == 1:
+    account = input("enter account name: ")
+    dict[account]
+    date = input("")
