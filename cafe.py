66bfb3b798e8c33fe39524f1a89556a540e290a3
cooldudeguyman <liamgray0912@gmail.com>
2021-08-02 19:46:53 +0800

    ive added up a lot of the inputs

--- cafe.py
+++ cafe.py
@@ -1,18 +1,28 @@
 #code = 01000101010001010100010010101
-
+tw = 0
 special = 1
-totals{
+totals = {
     "total orders": 0,
     "total days income": 0,
     "total GST": 0,
 }
 while special == 1:
-    # account = input("enter account name: ")
-    # dict[account]
-    # date = input("")
+    order = {}
     operation = input("What operation? ")
-    operation_lower = operation.lower()
-    if operation_lower == "order ":
-        account = input("enter account name: ")
-        dict[account]
-        date = input("")
+    operation = operation.lower()
+    if operation == "order":
+        account = input("enter name: ")
+        order[account]
+        time = input("time to pick up: ")
+        order["time of order"] = time
+        TorE = input("take away or eat in: ")
+        TorE = TorE.lower
+        order[TorE]
+        if TorE == "take away":
+            tw = tw = 1.05
+        print("""•	Cappuccino $3.00
+        •	Espresso $2.25
+        •	Latte $2.50
+        •	Iced Coffee $2.50""")
+        
+
