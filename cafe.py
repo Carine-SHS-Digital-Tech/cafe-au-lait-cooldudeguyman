79469044c34fa1354cc33f6b2f0b280baa11fafb
cooldudeguyman <liamgray0912@gmail.com>
2021-08-03 09:43:24 +0800

    doing something

--- cafe.py
+++ cafe.py
@@ -7,22 +7,37 @@ totals = {
     "total GST": 0,
 }
 while special == 1:
-    order = {}
+    order = {
+
+    }
     operation = input("What operation? ")
     operation = operation.lower()
     if operation == "order":
         account = input("enter name: ")
-        order[account]
+        order[account] = ""
         time = input("time to pick up: ")
         order["time of order"] = time
-        TorE = input("take away or eat in: ")
-        TorE = TorE.lower
-        order[TorE]
-        if TorE == "take away":
-            tw = tw = 1.05
-        print("""•	Cappuccino $3.00
-        •	Espresso $2.25
-        •	Latte $2.50
-        •	Iced Coffee $2.50""")
-        
+        error1 = 0
+        while error1 == 0:
+            TorE = input("enter number (1)take away or (2)eat in: ")
+            if TorE == 1:
+                tw = tw = 1.05
+                order["order type"] = TorE
+                error1 = error1 + 1
+            elif TorE == 2:
+                order["order type"] = TorE
+                error1 = error1 + 1
+            else:
+                print("miss input re enter")
+        print("""(1) Cappuccino $3.00
+        (2)	Espresso $2.25
+        (3)	Latte $2.50
+        (4)	Iced Coffee $2.50""")
+        morecoffee = 0
+        while morecoffee == 0:
+            coffee = input("what coffee do you want (enter number): ")
+            if coffee == 1:
+                order["Cappuccino"]
+            quantity = input(int("how many do you want: "))
+                   
 
